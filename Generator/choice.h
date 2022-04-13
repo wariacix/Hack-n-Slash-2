6a9a847b0f33865d436277f7b6266201b13c47f5
wariacix <wariasjakub@gmail.com>
2022-04-13 21:49:22 +0200

    Created GameObject and Interface classes mostly for future use, and implemented hp and mp bars using those classes. Code is pretty bad currently, subject to change for sure.

--- Generator/choice.h
+++ Generator/choice.h
@@ -5,8 +5,10 @@
 #include <string>
 #include "Map.h"
 #include "Player.h"
+#include "Interface.h"
+
 void drawInterface(sf::RenderWindow& window);
-void cityEnter(sf::RenderWindow& window, Map& mainMap, Player& mainPlayer);
+void cityEnter(sf::RenderWindow& window, Map& mainMap, Player& mainPlayer, equipmentSystem::Equipment &mainEquipment, hns::Interface& mainInterface);
 #ifndef choice_h
 #define choice_h
 int choice(sf::RenderWindow& window, std::wstring choiceString[6], int& choice);
